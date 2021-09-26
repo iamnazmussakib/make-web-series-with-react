6ff8603e9cd9835654fed0c0ee9c92147657c7d1
Nazmus Sakib <nazmussakibppp22@gmail.com>
2021-09-26 21:41:50 +0600

    issue fix

--- src/components/CharCount/CharCount.js
+++ src/components/CharCount/CharCount.js
@@ -5,14 +5,21 @@ const CharCount = (props) => {
     const charCount = props.charCount;
     let totalCost = 0;
     let charNames = [];
+    const unique = [];
     for(const character of charCount){
-        totalCost = totalCost + character.demand;
-        charNames.push(character.name);
+        if(unique.indexOf(character) === -1){
+            unique.push(character);
+            totalCost = totalCost + character.demand;
+            charNames.push(character.name);
+        }
+            
+        
+        
         
     }
     return (
         <div className="costing">
-            <h3>Character Count: {charCount.length}</h3>
+            <h3>Character Count: {unique.length}</h3>
             <h2>Total Cost: {totalCost}</h2>
             {
                 charNames.map(charName => <h4>{charName}</h4>)
